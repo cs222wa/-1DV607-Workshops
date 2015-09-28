99056e0c6574fd1160d3c5743d51586eadfa42fe
aa223gg <aa223gg@student.lnu.se>
2015-09-28 12:09:36 +0200

    Code

---
+++ Workshop 2 - Design/Workshop/model/Member.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Workshop.model
+{
+    class Member
+    {
+        private int id;
+        private string name;
+        private int personalIdentityNumber;
+        List<string> memberRegister;              
+
+        public Member()
+        {
+            id = 0;                                 //ID SKA EJ BÖRJA OM PÅ NOLL!
+            name = null;
+            personalIdentityNumber = 0;            
+            memberRegister = new List<string>();
+        }
+
+        public void RegisterMember(model.Member a_member, view.Console a_view, model.Menu a_menu)
+        {
+                a_member.name = a_view.RegisterName();
+                a_member.personalIdentityNumber = a_view.RegisterPersonalIdentityNumber();           
+                        
+                using (StreamWriter writer = new StreamWriter("memberRegister.txt", true))
+                {
+                    a_member.id++;    
+                    writer.WriteLine(a_member.id + "\t" + a_member.name + "\t" + a_member.personalIdentityNumber);
+                    writer.Close();
+                    a_view.ConfirmChange("Member registered: " + "Id: " + a_member.id + ", Name: " + a_member.name + ", Personal identity number: " + a_member.personalIdentityNumber);
+                    
+                    a_menu.ChooseFromMenu(a_member, a_menu);
+                }
+        }
+
+        public void ListMember(view.Console a_view)
+        {
+            int listType = a_view.ChooseListType();
+            using (StreamReader reader = new StreamReader("memberRegister.txt"))            //Hur visas VerboseList?
+            {                                                                               //Antal båtar läggas till i CompactList
+                string line = null;                             //Ska denna deklareras högst upp??
+                while ((line = reader.ReadLine()) != null)
+                {
+                    memberRegister.Add(line);
+                    a_view.ListAllMembers(line);
+                }                                                
+            }
+            
+        }                                                   //Fråga om quit eller ny meny
+
+        public void EditMember(view.Console a_view)
+        {
+            a_view.AskForMemberToEdit();
+        }
+        
+        public void ViewMember(view.Console a_view)
+        {            
+                ListMember(a_view);
+                int choosenMemberId = a_view.AskForMemberToView();
+                a_view.ViewSpecifikMember(memberRegister[choosenMemberId - 1]);
+                
+            
+        }
+
+
+        
+    }
+       
+}
---
+++ Workshop 2 - Design/Workshop/view/Console.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop.view
+{
+    class Console
+    {
+        public void Start()
+        {
+            System.Console.WriteLine("Press any key to view menu, q to quit.");
+        }
+
+        public bool Continue()
+        {
+            return System.Console.ReadKey().KeyChar != 'q';
+        }
+       
+         public int ViewMenu()           
+        {
+            System.Console.Clear();
+            System.Console.WriteLine("MENU:");
+            System.Console.WriteLine("1. Register new member");
+            System.Console.WriteLine("2. List all members");
+            System.Console.WriteLine("3. View specifik member");
+            System.Console.WriteLine("4. Edit member");
+            System.Console.WriteLine("5. Delete member");
+            System.Console.WriteLine("6. Register boat");
+            System.Console.WriteLine("7. Edit boat");
+            System.Console.WriteLine("8. Delete boat");
+            
+            return int.Parse(System.Console.ReadLine());
+        }
+
+         public void ConfirmChange(string prompt)
+         {
+             System.Console.WriteLine(prompt);
+         }
+         
+        public string RegisterName()
+         {
+             System.Console.Clear();
+             System.Console.WriteLine("REGISTER MEMBER");
+             System.Console.WriteLine("Name: ");
+             return System.Console.ReadLine();
+         }
+
+        public int RegisterPersonalIdentityNumber()
+        {
+            System.Console.WriteLine("Personal identity number: ");
+            return int.Parse(System.Console.ReadLine());
+        }
+
+        public int ChooseListType()
+        {
+            System.Console.WriteLine("Choose type of list:");
+            System.Console.WriteLine("1. Compact list");
+            System.Console.WriteLine("2. Verbose list");
+            return int.Parse(System.Console.ReadLine());
+        }
+
+        public void ListAllMembers(string line)
+        {
+            System.Console.WriteLine(line);            
+        }
+
+        public int AskForMemberToView()
+        {
+            System.Console.WriteLine("Choose member to view (memberID): ");
+            return int.Parse(System.Console.ReadLine());
+        }
+
+        public void ViewSpecifikMember(string member)
+        {
+            System.Console.WriteLine(member);
+        }
+
+        public void AskForMemberToEdit()
+        {
+            System.Console.WriteLine("Test editmember");
+        }
+    }
+}
